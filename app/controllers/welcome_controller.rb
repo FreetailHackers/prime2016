f3d396752745a26f2719837af0d33c19b3cde4f1
PrakharGarg <prakhargarg@utexas.edu>
2016-08-07 00:51:24 -0400

    rails app

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+  
+  def send_name
+    @name = params[:name]
+    url = 'http://git-garden.m3kpjgrp8e.us-west-2.elasticbeanstalk.com/api/'+ params[:name].to_s
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    @response = JSON.parse(response)
+    render 'show'
+  end 
+end

7f8723caf4e09ca58d71c7d0412fd18d89ccd4e3
leslierice <leslierice@utexas.edu>
2016-08-07 09:39:23 -0400

    return empty result set if user does not exist and add error checking

--- flask/application/__init__.py
+++ flask/application/__init__.py
@@ -25,43 +25,46 @@ def get_data(username):
     repo_names = []
     user_data = []
     today = date.today()
-    for repo in user_repos:
-        repo_name_parts = repo['svn_url'].split('/')
-        repo_names.append(repo_name_parts[-1])
-    logger.debug(repo_names)
-    for repo_name in repo_names:
-        cur_repo = {}
-        cur_repo['name'] = repo_name
-        logger.debug(repo_name)
-        streak = 0
-        repo_commits = requests.get('https://api.github.com/repos/{0}/{1}/commits?client_id={2}&client_secret={3}'.format(username, repo_name, application.config['GITHUB_CLIENT_ID'], application.config['GITHUB_CLIENT_SECRET'])).json()
-        cur_date = today
-        today_commit = False
-        for commit in repo_commits:
-            #TODO Need to check if commit matches user
-            last_commit = commit['commit']['committer']['date'][0:10]
-            last_commit_date = datetime.strptime(last_commit, '%Y-%m-%d').date()
-            days = (cur_date - last_commit_date).days
-            logger.debug(days)
-            #TODO Need to check correctness
-            if days < 0:
-                break
-            elif days == 0 or days == 1:
-                if days == 0:
-                    today_commit = True
-                    streak = 1
-                else:
-                    cur_date = cur_date + timedelta(days=-1)
-                    streak += 1
+    if u'message' in user_repos:
+            logger.debug('No user found.')
+    else:
+        for repo in user_repos:
+            if 'svn_url' in repo:
+                repo_name_parts = repo['svn_url'].split('/')
+                repo_names.append(repo_name_parts[-1])
+        for repo_name in repo_names:
+            cur_repo = {}
+            cur_repo['name'] = repo_name
+            streak = 0
+            repo_commits = requests.get('https://api.github.com/repos/{0}/{1}/commits?client_id={2}&client_secret={3}'.format(username, repo_name, application.config['GITHUB_CLIENT_ID'], application.config['GITHUB_CLIENT_SECRET'])).json()
+            cur_date = today
+            today_commit = False
+            for commit in repo_commits:
+                #TODO Need to check if commit matches user
+                try:
+                    last_commit = commit['commit']['committer']['date'][0:10]
+                    last_commit_date = datetime.strptime(last_commit, '%Y-%m-%d').date()
+                    days = (cur_date - last_commit_date).days
+                    if days < 0:
+                        break
+                    elif days == 0 or days == 1:
+                        if days == 0:
+                            today_commit = True
+                            streak = 1
+                        else:
+                            cur_date = cur_date + timedelta(days=-1)
+                            streak += 1
+                    else:
+                        streak -= days
+                        break
+                except:
+                    continue
+            cur_repo['days'] = streak
+            if streak < 0:
+                cur_repo['image'] = images[0]
+            elif streak > 6:
+                cur_repo['image'] = images[6]
             else:
-                streak -= days
-                break
-        cur_repo['days'] = streak
-        if streak < 0:
-            cur_repo['image'] = images[0]
-        elif streak > 6:
-            cur_repo['image'] = images[6]
-        else:
-            cur_repo['image'] = images[streak]
-        user_data.append(cur_repo)
+                cur_repo['image'] = images[streak]
+            user_data.append(cur_repo)
     return jsonify({'results': user_data})

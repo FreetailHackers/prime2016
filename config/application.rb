f9bc53827312a516ed5995227a7c3c3fe75dd38c
PrakharGarg <prakhargarg@utexas.edu>
2016-08-07 12:59:28 -0400

    ima cry

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,9 @@ module GitTree
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
   end
 end

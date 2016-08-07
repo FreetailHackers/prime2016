0cf3b9ec504b3f0f40e3e1b5d0af63e377bf63ad
PrakharGarg <prakhargarg@utexas.edu>
2016-08-07 02:58:51 -0400

    stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,6 +10,7 @@ class WelcomeController < ApplicationController
     url = 'http://git-garden.m3kpjgrp8e.us-west-2.elasticbeanstalk.com/api/'+ params[:name].to_s
     uri = URI(url)
     response = Net::HTTP.get(uri)
+    binding.pry
     @response = JSON.parse(response)
     render 'show'
   end 
